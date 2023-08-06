fc73a27313076b0e5815be9206e3a9cac3ec48aa
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-08-05 22:42:59 -0700

    clients

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+"use client"
+import Image from 'next/image'
 
 
 const CustomButton = () => {
   return (
-    <div>CustomButton</div>
+    <button
+    disabled={false}
+    type={"button"}
+    className={`custom-btn`}
+    onClick={()=>{}}
+    >
+      <span className={`flex-1`}>
+        title
+      </span>
+    </button>
   )
 }
 
