7a2c3675b6f2d7f1ff63e7f5b5e40811b560bb2d
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-08-10 00:45:15 -0700

    filter

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,10 +1,15 @@
 "use client";
+
+import { useState, Fragment } from 'react';
 import { Combobox, Transition} from '@headlessui/react'
 import Image from 'next/image';
 
 import { SearchManufacturerProps } from '@/types'
 const SearchManufacturer = ({manufacturer, 
   setManufacturer}: SearchManufacturerProps) => {
+    const [query, setQuery] = useState('')
+
+    
   return (
     <div className='search-manufacturer'>
 
@@ -20,6 +25,24 @@ const SearchManufacturer = ({manufacturer,
             />
           </Combobox.Button>
 
+          <Combobox.Input
+          className="search-manufacturer__input"
+          placeholder='volkswagen'
+          displayValue={(manufacturer: string)=>manufacturer}
+          onChange={(e)=>setQuery(e.target.value)}
+          />
+          <Transition
+          as={Fragment}
+          leave='transition ease-in duration-100'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'
+          afterLeave={()=>setQuery('')}
+          >
+            <Combobox.Options>
+
+            </Combobox.Options>
+
+          </Transition>
         </div>
       </Combobox>
     </div>
