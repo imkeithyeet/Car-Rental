79093c7654c49f9728a46e67280f2499e71c8d16
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-09-01 23:36:26 -0700

    words

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,11 +1,24 @@
 "use client"
 import { useState } from "react"
 import { SearchManufacturer } from "./"
+import Image from "next/image"
 
-
+const SearchButton =({otherClasses}:{ otherClasses:string})=>{
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+    src="/magnifying-glass.svg"
+    alt="magnifying glass"
+    width={40}
+    height={40}
+    className="object-contain"
+    />
+    </button>
+}
 const SearchBar = () => {
 const [manufacturer, setManufacturer] = useState("")
+
     const handleSearch = ()=>{}
+
   return (
     <form className="searchbar" onSubmit=
     {handleSearch}>
@@ -14,6 +27,10 @@ const [manufacturer, setManufacturer] = useState("")
         manufacturer={manufacturer}
         setManufacturer={setManufacturer}
         />
+        <SearchButton otherClasses="sm:hidden"/>
+        </div>
+        <div className="searchbar__item">
+
         </div>
     </form>
   )
