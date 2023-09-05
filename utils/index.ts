85ad67f7c30627d5cad4f6b40f225fa40a527afd
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-09-04 21:48:19 -0700

    stuff

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,13 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function  fetchCars(){
+export async function  fetchCars( filters: FilterProps ){
+  const {manufacturer, year, model, limit, fuel} = filters;
     const headers ={
 		'X-RapidAPI-Key': '1e9e26f426mshd4b6b8ee85afb71p1e8327jsn5844bebe0449',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3',{
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3`,{
         headers:headers,
     });
     const result= await response.json()
