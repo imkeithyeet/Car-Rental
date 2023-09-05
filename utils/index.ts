3ab8485e330ef3fef443c9de7d3604697044ef3c
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-09-05 00:31:51 -0700

    filters

--- utils/index.ts
+++ utils/index.ts
@@ -7,9 +7,12 @@ export async function  fetchCars( filters: FilterProps ){
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
 
-    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3`,{
-        headers:headers,
-    });
+  const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+    {
+      headers: headers,
+    }
+  );
     const result= await response.json()
 
     return result
