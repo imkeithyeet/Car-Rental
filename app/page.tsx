fc5738ffa42f87b2cefc2acc71450f5af705b9ee
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-09-10 16:33:49 -0700

    moreprops

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { CarCard, CustomFilter, Hero, SearchBar } from '@/components'
+import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from '@/components'
 import { fuels, yearsOfProduction } from '@/constants'
 import { fetchCars } from '@/utils'
 import Image from 'next/image'
@@ -31,13 +31,20 @@ export default async function Home({ searchParams }) {
           </div>
         </div>
 
-      {!isDataEmpty?(
+      {!isDataEmpty ? (
         <section>
           <div className='home__cars-wrapper'>
             {allCars?.map((car)=> (
             <CarCard car={car} />
             ))}
           </div>
+          <ShowMore
+          pageNumber={(searchParams.pageNumber || 
+            10) / 10}
+            isNext={(searchParams.limit || 10) >
+            allCars.length}
+
+          />
         </section>
       ):(
         <div className='home__error-container'> 
